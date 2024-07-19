2c0ac3a21c745e9bd79833a2f8ede6899ac46575
silviapoliveira <correiodasilviaoliveira@gmail.com>
2024-07-19 11:20:08 +0100

    First controller

---
+++ Supershop/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Supershop.Data.Entities;
+
+namespace Supershop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
