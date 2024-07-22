d7dbb82d323642a5161c4ffca4681ddf914489b4
silviapoliveira <correiodasilviaoliveira@gmail.com>
2024-07-22 14:15:18 +0100

    Modify products

--- Supershop/Data/Entities/Product.cs
+++ Supershop/Data/Entities/Product.cs
@@ -8,6 +8,8 @@ namespace Supershop.Data.Entities
     {
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage = "The field {0} can contain {1} characters length.")]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -17,10 +19,10 @@ namespace Supershop.Data.Entities
         public string ImageUrl { get; set; }
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
         [Display(Name = "Is Available")]
         public bool IsAvailable { get; set; }
