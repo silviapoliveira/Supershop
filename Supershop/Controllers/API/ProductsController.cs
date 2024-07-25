f29f81ac4d650c24ceb0f79d57cf6b9279980c98
silviapoliveira <correiodasilviaoliveira@gmail.com>
2024-07-25 18:21:29 +0100

    API products controller

---
+++ Supershop/Controllers/API/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Supershop.Data;
+
+namespace Supershop.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_productRepository.GetAll());
+        }
+    }
+}
